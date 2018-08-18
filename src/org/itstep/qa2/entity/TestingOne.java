bf71f6f928435be1947d6cd22eeaa9412f4e847f
qa <qa.mail.ru>
2018-08-18 20:47:58 +0300

    Initial commit

---
+++ src/org/itstep/qa2/entity/TestingOne.java
@@ -0,0 +1,11 @@
+package org.itstep.qa2.entity;
+
+public class TestingOne {
+    public static void main(String[] args) {
+        int testVar;
+        testVar = 4;
+        int testThree;
+        testThree = (testVar * 2);
+        System.out.println(testThree);
+    }
+}
